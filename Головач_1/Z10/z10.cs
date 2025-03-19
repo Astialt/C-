bc3d7d1ee7b8c158891c05f3ee841b9724ebc127
KsGl <golksenia6@gmail.com>
2025-03-19 10:47:00 +0300

    Add 1

---
+++ Головач_1/Z10/z10.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        bool isAscending = true;
+        int lastDigit = number % 10;
+        number /= 10;
+
+        while (number > 0)
+        {
+            int currentDigit = number % 10;
+            if (currentDigit < lastDigit)
+            {
+                isAscending = false;
+                break;
+            }
+            lastDigit = currentDigit;
+            number /= 10;
+        }
+
+        if (isAscending)
+        {
+            Console.WriteLine("Цифры числа упорядочены по возрастанию справа налево.");
+        }
+        else
+        {
+            Console.WriteLine("Цифры числа не упорядочены по возрастанию справа налево.");
+        }
+    }
+}
---
+++ Головач_1/Z3/z3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число A (1 <= A < B <= 100): ");
+        int A = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите число B (A < B <= 100): ");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        if (A >= 1 && B <= 100 && A < B)
+        {
+            int sum = 0;
+            for (int i = A; i <= B; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine($"Сумма всех целых чисел от {A} до {B} включительно: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: некорректный ввод чисел.");
+        }
+    }
+}
